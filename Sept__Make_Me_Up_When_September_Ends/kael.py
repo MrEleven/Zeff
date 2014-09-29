c8a5a7488684e6159a86c21c10e5d9a9d2b5c9ea
yumi <iGod_eleven@163.com>
2014-09-29 17:23:41 +0800

    add caesar decorator

--- Sept__Make_Me_Up_When_September_Ends/kael.py
+++ Sept__Make_Me_Up_When_September_Ends/kael.py
@@ -24,6 +24,34 @@ def kael(func):
 @kael
 def say(a={"adef": 1, "atype": int}, b={"adef": 'x', 'atype': str}, c={'adef': 'ok', 'atype': str}):
     print 'a =', repr(a), 'b =', repr(b), 'c =', repr(c)
+
+def caesar(func):
+    arg_info = inspect.getargspec(func)
+    func_args = list(arg_info.args)
+    has_self = "self" in func_args
+    if has_self:
+        func_args.pop(0)
+    defaults = arg_info.defaults
+    default_args = { func_args[i-len(defaults)]: v for i, v in enumerate(defaults) }
+    def __monkey(*args, **kwargs):
+        args = list(args)
+        if has_self:
+            self = args.pop(0)
+        monkey_map = { func_args[i]: arg for i, arg in enumerate(args) }
+        kwargs.update(monkey_map)
+        monkey_map = {}
+        monkey_map.update(default_args)
+        monkey_map.update(kwargs)
+        print "*[*caesar args*]*  ", monkey_map, "  *[*/caesar args*]*"
+        if has_self:
+            result = func(self, **monkey_map)
+        else:
+            result = func(**monkey_map)
+        print "*[*caesar return*]*  ", result, "  *[*/caesar return*]*"
+        return result
+    return __monkey
+
+
         
 if __name__ == '__main__':
     say("1", c=1)
