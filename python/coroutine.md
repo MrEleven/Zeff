2bad776fea9000ece182e10c3b5e98e0859c1753
eleven <iGod_eleven@163.com>
2015-04-02 20:45:09 +0800

    add something about coroutine

---
+++ python/coroutine.md
@@ -0,0 +1,3 @@
+最近发现一个很好玩的东西叫corountine(协程)。蛮有意思的。最近多学学gevent这些东西。
+
+url: http://blog.jobbole.com/77240/
--- python/producter_consumer.py
+++ python/producter_consumer.py
@@ -1,5 +1,7 @@
 """
     对coroutine这一块还不是很清楚。今天回家以后好好看看这个。据说Go和Erlang都是靠这个实现并发的。等学会了这个再去看看nodejs的callback模型
+    The only difference is that a generator function cannot control where should the execution continue after it yields; the control is always transferred to the generator’s caller. 
+    无法决定挂起之后的生成器的执行控制权限应该分配给谁是python生成器方式coroutine的硬伤啊。不过生成器跟coroutine本身就是两个概念
 """
 
 queue = []
